9fd26e14c6322583282340d0f70cddca0b72be91
TAUSIF <mdtausif0289@gmail.com>
2022-08-25 20:31:36 +0530

    Project updated

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -111,6 +111,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR,'statticfiles')
+
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [BASE_DIR / "static"]
